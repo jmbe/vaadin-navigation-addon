e97acf0b336460d79d58bcaa30a4b10b910e37d4
J-M Bergqvist <jm.bergqvist@gmail.com>
2010-10-27 22:39:11 +0200

    Cache activities based on class, so that there will be only one activity instance even if it is registered for several places

--- src/main/java/com/vaadin/addon/navigation/core/CachingActivityRepository.java
+++ src/main/java/com/vaadin/addon/navigation/core/CachingActivityRepository.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Maps;
 import com.vaadin.addon.navigation.activity.Activity;
-import com.vaadin.addon.navigation.activity.ActivityMapper;
 import com.vaadin.addon.navigation.api.NavigableApplication;
 import com.vaadin.addon.navigation.place.Place;
 
@@ -15,35 +14,29 @@ import com.vaadin.addon.navigation.place.Place;
  * ActivityMapper implementation which caches the created activities. Useful if the activities are used as combined
  * presenters and views (as is common in Vaadin).
  */
-public class CachingActivityRepository implements ActivityMapper {
+public class CachingActivityRepository extends ActivityFactory {
 
     /** Logger for this class. */
     private static final Logger log = LoggerFactory.getLogger(CachingActivityRepository.class);
 
-    private Map<Class<? extends Place>, Activity> classToActivity;
+    private Map<Class<? extends Activity>, Activity> classToInstance;
 
-    private ActivityMapper activityMapper;
-
-    public CachingActivityRepository(final ActivityMapper activityMapper) {
-        this.classToActivity = Maps.newHashMap();
-        this.activityMapper = activityMapper;
+    public CachingActivityRepository() {
+        this.classToInstance = Maps.newHashMap();
     }
 
     @Override
     public Activity get(final Place place, final NavigableApplication application) {
         log.debug("Looking up activity for place {}", place);
 
-        if (!classToActivity.containsKey(place.getClass())) {
-            Activity activity = activityMapper.get(place, application);
-            classToActivity.put(place.getClass(), activity);
+        Class<? extends Activity> activityClass = getActivityClass(place);
+
+        if (!classToInstance.containsKey(activityClass)) {
+            Activity activity = super.get(place, application);
+            classToInstance.put(activityClass, activity);
         }
 
-        return classToActivity.get(place.getClass());
-    }
-
-    @Override
-    public void register(final Class<? extends Place> place, final Class<? extends Activity> activity) {
-        this.activityMapper.register(place, activity);
+        return classToInstance.get(activityClass);
     }
 
 }
